9b35c556824fa264d0811fbb781ea1db6d3c22c3
BFourDoubleL <115538331+BFourDoubleL@users.noreply.github.com>
2024-12-24 21:45:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
 const API_KEY = "AIzaSyAVc-Z1qyfhred2j-mu3knchPiaCuDRBfc";
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key=${API_KEY}`;
 
 
 // Initialize user message and file data
