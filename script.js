991427cc75a7df8fc31cb920aed4145d9c832adb
BFourDoubleL <115538331+BFourDoubleL@users.noreply.github.com>
2025-10-28 11:03:11 +0200

    Refactor chat script for API and message handling
    Updated API URL and improved chat handling logic.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
 const API_KEY = "AIzaSyDj_mbQ2jvWYp3H85xzwKjFRsOdQ_jG-RM";
-const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 
 // Initialize user message and file data
@@ -187,6 +187,7 @@ sendMessage.addEventListener("click", (e) => handleOutgoingMessage(e));
 document.querySelector("#file-upload").addEventListener("click", () => fileInput.click());
 closeChatbot.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
 chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+
 
 
 
