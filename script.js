1c740a6181e7fd1b0bd38a630bfc21ed66a58280
BFourDoubleL <115538331+BFourDoubleL@users.noreply.github.com>
2025-10-30 12:02:55 +0200

    Refactor chat script and improve API handling

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function extractTextFromResponse(data) {
   }
 }
 
-function typeTextEffect(element, text, speed = 10) {
+function typeTextEffect(element, text, speed = 2p) {
   let index = 0;
   const interval = setInterval(() => {
     element.textContent += text.charAt(index);
@@ -235,6 +235,7 @@ sendMessage.addEventListener("click", (e) => handleOutgoingMessage(e));
 document.querySelector("#file-upload").addEventListener("click", () => fileInput.click());
 closeChatbot.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
 chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+
 
 
 
